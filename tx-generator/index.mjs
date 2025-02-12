f5356c463fd024200aa6caa1af11c2eb7cbf157b
JEAN GAL <jgatbkk@gmail.com>
2025-02-12 17:45:29 -0500

    upload index.mjs for sending a tx

---
+++ tx-generator/index.mjs
@@ -0,0 +1,11 @@
+import { ethers } from "ethers";
+(async () => {
+    const provider = new ethers.JsonRpcProvider('YOUR_ROLLUPS_RPC_URL');
+    const signer = new ethers.Wallet('FUNDED_PRIVATE_KEY_ON_YOUR_ROLLUP', provider);
+
+    const tx = await signer.sendTransaction({
+        to: 'RECIPIENT_ADDRESS',
+        value: ethers.parseUnits('1', 'wei'),
+    });
+    console.log(tx);
+})();
