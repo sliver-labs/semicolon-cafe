48922b64d89e607adf775776503385728995d7ef
JEAN GAL <jgatbkk@gmail.com>
2025-03-05 17:31:43 -0500

    Update the tx generator script info

--- tx-generator/index.mjs
+++ tx-generator/index.mjs
@@ -1,13 +1,13 @@
 import { ethers } from "ethers";
 (async () => {
-    const provider = new ethers.JsonRpcProvider('http://127.0.0.1:8547'); // Rollup RPC URL
-    const signer = new ethers.Wallet('FUNDED_PRIVATE_KEY_ON_YOUR_ROLLUP', provider);
+    const provider = new ethers.JsonRpcProvider('http://YOUR_ROLLUP_RPC_URL:8547'); // Rollup RPC URL
+    const signer = new ethers.Wallet('YOUR_PRIVATE_KEY', provider);
 
     const tx = await signer.sendTransaction({
-        to: 'RECIPIENT_ADDRESS',
+        to: 'DESTINATION_ADDRESS',
         value: ethers.parseUnits('1', 'wei'),
         gasPrice: ethers.parseUnits('1', 'gwei'),
-        gasLimit: 24000,
+        gasLimit: 26000,
     });
     console.log(tx);
 })();
